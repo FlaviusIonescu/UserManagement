69b024e512b5576f52165eee883c135960d4dd14
Flavius Ionescu <flavius.ionescu13@gmail.com>
2023-01-27 20:25:43 +0100

    [fe]added email validation

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { UserService } from '../service/user.service';
 import { User } from '../model/user';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -13,17 +14,33 @@ export class LoginComponent implements OnInit {
 
   user: User = new User();
 
+  loginForm:FormGroup =  new FormGroup({
+    email: new FormControl('',[
+      Validators.required,
+      Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]),
+    password: new FormControl('')
+    });
+
+
   constructor(
       private route: ActivatedRoute,
       private router: Router,
       private userService: UserService
-  ) { }
+  ) {
+    
+   }
+
+get getEmail(){
+  return this.loginForm.get('email');
+ }
 
   ngOnInit() {
       sessionStorage.setItem('token', '');
   }
 
   doLogin() {
+    this.user.username = this.loginForm.value.email || '';
+    this.user.password = this.loginForm.value.password || '';
     this.userService.login(this.user).subscribe(data => {
       if (data) {
         sessionStorage.setItem(
